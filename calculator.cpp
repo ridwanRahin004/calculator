d1b53a08daf0da644c487a1224de73646d0ebb14
ridwanRahin004 <92874151+ridwanRahin004@users.noreply.github.com>
2021-12-16 17:17:56 +0600

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,38 +1,114 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
+double num1,num2,answer;
+char op;
+int cont_or_exit;
+double pl();
+double min();
+double as();
+double by();
 int main()
 {
-  /*Building a normal calculetor*/
-  double num1;
-  double num2;
-  char op;
-  double answer;
-  cout<<"Enter first number: "<<endl;
-  cin>>num1;
-  cout<<"Enter second number: "<<endl;
-  cin>>num2;
-  cout<<"Enter oparetor: "<<endl;
-  cin>>op;
-  if (op=='+')
-  {
+    cout<<"Enter first number: "<<endl;
+    cin>>num1;
+    cout<<"Enter oparetor: "<<endl;
+    cin>>op;
+    cout<<"Enter second number: "<<endl;
+    cin>>num2;
+    if(op=='+')
+    {
+        pl();
+    }
+    else if(op=='-')
+    {
+        min();
+    }
+    else if(op='*')
+    {
+        as();
+    }
+    else if(op=='/')
+    {
+        by();
+    }
+    else{
+        cout<<"\n#Try again"<<endl;
+        cout<<"[1] try again"<<endl;
+        cout<<"[0] exit"<<endl;
+        cin>>cont_or_exit;
+        if(cont_or_exit==1)
+        {
+            main();
+        }
+        else if(cont_or_exit==0)
+        {
+            cout<<"\n\n\t#BYE!"<<endl;
+        }
+    }
+    return 0;
+}
+double pl(){
     answer=num1+num2;
-    cout<<"Asnwer: "<<answer<<endl;
-  }
-  else if(op=='-')
-  {
+    cout<<"Answer: "<<answer<<endl;
+    cout<<"\n"<<endl;
+    cout<<"[1] try again"<<endl;
+    cout<<"[0] exit"<<endl;
+    cin>>cont_or_exit;
+    if(cont_or_exit==1)
+    {
+        main();
+    }
+    else if(cont_or_exit==0)
+    {
+        cout<<"\n\n\t#BYE!"<<endl;
+    }
+}
+double min(){
     answer=num1-num2;
     cout<<"Answer: "<<answer<<endl;
-  }
-  else if(op=='*')
-  {
+    cout<<"\n"<<endl;
+    cout<<"[1] try again"<<endl;
+    cout<<"[0] exit"<<endl;
+    cin>>cont_or_exit;
+    if(cont_or_exit==1)
+    {
+        main();
+    }
+    else if(cont_or_exit==0)
+    {
+        cout<<"\n\n\t#BYE!"<<endl;
+    }
+}
+double as(){
     answer=num1*num2;
     cout<<"Answer: "<<answer<<endl;
-  }
-  else if(op=='/')
-  {
+    cout<<"\n"<<endl;
+    cout<<"[1] try again"<<endl;
+    cout<<"[0] exit"<<endl;
+    cin>>cont_or_exit;
+    if(cont_or_exit==1)
+    {
+        main();
+    }
+    else if(cont_or_exit==0)
+    {
+        cout<<"\n\n\t#BYE!"<<endl;
+    }
+}
+double by(){
     answer=num1/num2;
     cout<<"Answer: "<<answer<<endl;
-  }
-  return 0;
+    cout<<"\n"<<endl;
+    cout<<"[1] try again"<<endl;
+    cout<<"[0] exit"<<endl;
+    cin>>cont_or_exit;
+    if(cont_or_exit==1)
+    {
+        main();
+    }
+    else if(cont_or_exit==0)
+    {
+        cout<<"\n\n\t#BYE!"<<endl;
+    }
 }
